c879798aaaf4d3fe58484739d73e9686b3c9b9b2
Raviraj Bhat <ravirajbhat154@gmail.com>
2023-06-21 17:54:51 +0000

    :tada: Initial commit

---
+++ sec_edger_harvester/__init__.py
@@ -0,0 +1,15 @@
+# type: ignore[attr-defined]
+"""Python library to harvest target exhibit files list from sec edger database that matches given list of phrases"""
+
+import sys
+from importlib import metadata as importlib_metadata
+
+
+def get_version() -> str:
+    try:
+        return importlib_metadata.version(__name__)
+    except importlib_metadata.PackageNotFoundError:  # pragma: no cover
+        return "unknown"
+
+
+version: str = get_version()
